af35c68f87556e43cce01f4140a845bef8b131c2
Nicola Matera <nicolamateraposta@gmail.com>
2026-07-01 17:44:05 +0200

    fix pages 3.0

--- whitelabelsite/vite.config.js
+++ whitelabelsite/vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/git-tutorial/', 
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    // Dice a Vite di salvare i file pronti un livello più in alto (../), 
+    // ovvero direttamente nella root del tuo repository GitHub
+    outDir: '../docs', 
+  }
 })
